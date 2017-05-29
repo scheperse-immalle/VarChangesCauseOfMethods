9cbf541ddde8745bc835a7141583edbe2df279b6
eline.schepers <eline.schepers@B209-04>
2017-05-29 11:02:50 +0200

    Adding class MyClass

---
+++ VariablenVeranderenMbvMethod/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablenVeranderenMbvMethod
+{
+    class MyClass
+    {
+        public string Beschrijving { get; set; }
+        public int Hoeveelheid { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Beschrijving: [{0}] - Hoeveelheid: [{1}]", Beschrijving, Hoeveelheid);
+        }
+    }
+}
